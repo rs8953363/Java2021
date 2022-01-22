e9907508a713157678ac6bec1dda764324672ff3
Rose <rs8953363@gmail.com>
2022-01-21 20:50:31 -0600

    ...

--- src/LocalInstanceStatic5/Static2.java
+++ src/LocalInstanceStatic5/Static2.java
@@ -1,6 +1,13 @@
 package LocalInstanceStatic5;
 
 public class Static2 {
-    int speed = 200;//Unless you create and object for this class, speed and color variables are not added to the memory
+    int speed = 200;//Unless you create and object for this class, speed and color variables are not added to the memory and you cannot use them.
     String color = "Blac";
+    static  int model =2015;//This is added to the memory
+
+    public static void Information(){//This method is added to memory as well.
+        //System.out.println(speed); There cannot be a non-static variable in static method. This line gives compilation
+        // error. If you want to use speed and color in this method you need to either make them static or make this method
+        // non-static.
+    }
 }
--- src/LocalInstanceStatic5/Static2Demo.java
+++ src/LocalInstanceStatic5/Static2Demo.java
@@ -2,10 +2,19 @@ package LocalInstanceStatic5;
 
 public class Static2Demo {
 
-    int number = 99;
+    int number = 99;//This number variable and main method is created in the memory
     public static void main(String[] args) {
         System.out.println("Main method");
+        System.out.println(Static2.model);//You can call model variable by using class name, because, it's static.
+    //First static variables, methods and main method(as it's static) are added to the memory before program starts  working.
+        // Then the program starts. And then it creates the number variable in the memory.
+        //If you create an object, It adds the other variables (seed and color) to the memory.
+        //System.out.println(number); You cannot call a non-static variable in static main method.If you want to use it you need to make variable static.
+   //deneme(); You cannot call this non-static method in static main class.
+    }
+    public void test(){
+
     }
 }
 //The static method can not use non static data member or call non-static method directly.
-//this and super cannot be used in static context.
+//this and super cannot be used in static context. A non-static method cannot be used in static method.
