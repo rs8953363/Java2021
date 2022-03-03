081188c21c6a3db7b2be480dde6aef9549318f68
Rose <rs8953363@gmail.com>
2022-03-02 21:47:56 -0600

    ...

---
+++ src/SwitchStatements/BMI.java
@@ -0,0 +1,50 @@
+package SwitchStatements;
+
+public class BMI {
+
+        public static void main(String[] args) {
+            /*
+             * Write a Java program that accepts name,surname, years, weight, height;
+             * Calculates BMI(Body Mass Index) and prints the user's status like that;
+             * John CARPENTER is 25 years old, his height is 170.5 cm and his weight is 80.24 kg. He is HEALTHY because his BMI=24.7
+             * formula; bmi = weight / (height * height / 10000);
+             * bmi less than or equal 20 is weak,less than or equal 25 is healty,less than or equal 30 is fat, the other option is obese
+             */
+
+            String name="John";
+            String surname="CARPENTER";
+            int years=25;
+            double height=170.5;
+            double weight=80.24;
+            double bmi=weight / (height * height / 10000);
+
+            String status;
+            if(bmi<=20) {
+                status="weak";
+            }else if(bmi<=25) {
+                status="healty";
+            }else if(bmi<=30) {
+                status="fat";
+            }else
+                status="obese";
+
+            System.out.println(name + " " + surname + " is " + years + " years old ,his height is " + height
+                    + " and his weight is " + weight + " He is " + status + " because his BMI=" + bmi);
+
+
+
+            System.out.printf(
+                    "%s %S is %d years old, his height is %.1f cm and his weight is %.2f kg. He is %S because his BMI=%.1f",
+                    name, surname, age, height, weight, status, bmi);
+            /*
+             * % Format Specifier %s , %S formats strings %f formats the floating-point
+             * numbers %d formats decimal integers
+             *
+             * %[flags][width][.precision]S https://www.baeldung.com/java-printstream-printf
+             */
+
+        }
+
+
+    }
+
