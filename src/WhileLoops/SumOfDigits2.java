1f722cdaca3066f943cb2159a158feaa8e5d9d6d
Rose <rs8953363@gmail.com>
2022-03-30 22:34:53 -0500

    ...

---
+++ src/WhileLoops/SumOfDigits2.java
@@ -0,0 +1,19 @@
+package WhileLoops;
+
+public class SumOfDigits2 {
+
+    public static void main(String[] args) {
+        // ) Kullanicidan bir sayi alin ve
+        // bu sayinin rakamlari toplamini yazdirin
+        int input = 15687456;
+        int rakam=0;
+        int rakamlartoplami=0;
+        while(input>0){
+            rakam=input%10;
+            rakamlartoplami += rakam;
+            input/=10;
+        }
+        System.out.println(rakamlartoplami);
+    }
+}
+
