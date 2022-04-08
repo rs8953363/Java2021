d9208c388b8d2de2d4ef4e0ef30af4b23e7b6a55
Rose <rs8953363@gmail.com>
2022-04-08 00:09:07 -0500

    ...

---
+++ src/Variables/LocalVariables.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class LocalVariables {
+    public class C03_LocalVariables {
+        public static void main(String[] args) {
+            int sayi=0;
+            for (int i = 0; i <10 ; i++) {
+                System.out.println(sayi+1);
+            }
+        }
+    }
+
+}
