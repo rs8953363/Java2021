6e6a4d2f33eb794741f59f83d17d7e1cc44b3069
Rose <rs8953363@gmail.com>
2022-04-16 05:51:09 -0500

    ...

---
+++ src/Constructor4/ConstructorTypes.java
@@ -0,0 +1,26 @@
+package Constructor4;
+
+public class ConstructorTypes {
+
+        public ConstructorTypes(String par1){
+            System.out.println("parametreli");
+        }
+        ConstructorTypes(){
+            System.out.println("parametresiz");
+        }
+
+            // Cons01 obj1= new Cons01(); // default constructor devrede
+        /* Cons01 class'inda hic constructor olusturmazsak,
+           Java default constructor'i kullandigindan
+           obj1'i uretebiliriz
+           Ancak biz parametreli vea parametresiz bir constructor yazdigimizda
+           Java default constructor'i siler.
+           Dolayisiyla biz herhangi bir constructor olusturdugumuzda
+           daha once baska claslar veya kullanicilarin
+           olusturmus olabilecegi objeleri kullanabilmeleri icin
+           default constructor'in islevini gerceklestirecek
+           parametresiz bir constructor olusturmakta fayda var
+         */
+         //   Cons01 obj2=new Cons01("Java");
+        }
+
---
+++ src/Constructor4/Toyota.java
@@ -0,0 +1,12 @@
+package Constructor4;
+
+public class Toyota {
+        String marka="Toyota";
+        int tekerAdedi=4;
+        boolean motoruVarMi=true;
+        String model;
+        String yakit;
+        int yil;
+    }
+
+
