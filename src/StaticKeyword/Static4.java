5cb850e830d1e3f558643625bf2b48ce5879b3d5
Rose <rs8953363@gmail.com>
2022-04-22 01:00:44 -0500

    ...

---
+++ src/StaticKeyword/Static4.java
@@ -0,0 +1,11 @@
+package StaticKeyword;
+
+public class Static4 {
+        static int sayi=5;
+        public static void artirma(){
+            sayi++;
+            System.out.println("sayinin su andaki degeri : " + sayi);
+        }
+    }
+
+
