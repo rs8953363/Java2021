5261a1eec21eb7586cbb5453f783667d34c582e6
Rose <rs8953363@gmail.com>
2022-05-05 00:01:24 -0500

    ...

---
+++ src/Inheritance/Isci2.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+    public class Isci2 extends Muhasebe{
+        protected String isciStatu ="Surekli Isci";
+        protected void mesai(){
+            System.out.println("Isciler gunluk 8 saat ve haftada 5 gun calisir");
+        }
+    }
+
