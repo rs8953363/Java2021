c6c172688b2bb333dce23ef0ac8b3f1311594a0b
Rose <rs8953363@gmail.com>
2022-05-06 23:15:25 -0500

    ...

---
+++ src/Inheritance/Parent3.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Parent3 {
+        protected Parent3(){
+            System.out.println("Parent parametresiz cons. calisti");
+        }
+        protected Parent3(int sayi3, int sayi4){
+            System.out.println("Parent 2 parametreli cons");
+        }
+        protected int sayi;
+    }
+
