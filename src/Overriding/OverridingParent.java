1638e230fffd59237e0bd1d4af5b5adeec4c9180
Rose <rs8953363@gmail.com>
2022-05-08 05:24:21 -0500

    ...

---
+++ src/Overriding/OverridingParent.java
@@ -0,0 +1,10 @@
+package Overriding;
+
+public class OverridingParent {
+    public void method1(){
+        System.out.println("Parent class method1");
+    }
+    public void method2(){
+        System.out.println("Parent class method2");
+    }
+}
