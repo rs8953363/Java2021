2a7deab53f0daa4e86a16f4f25f64df651f0e842
Rose <rs8953363@gmail.com>
2022-05-16 00:32:11 -0500

    ...

---
+++ src/AbsractClassInterfece/C02_Toyota.java
@@ -0,0 +1,11 @@
+package AbsractClassInterfece;
+
+public class C02_Toyota extends C01_AbstractClass{
+    @Override
+    public void motor() {
+    }
+    @Override
+    public void vites() {
+    }
+}
+
