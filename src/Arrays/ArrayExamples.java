9d4c32b7cf663a2f9c6874652aef9cb2ccb0de6c
Rose <rs8953363@gmail.com>
2022-06-11 23:59:22 -0500

    ...

--- src/Arrays/ArrayExamples.java
+++ src/Arrays/ArrayExamples.java
@@ -5,5 +5,15 @@ public class ArrayExamples {
         int schoolmarks [] = {25, 30, 50, 10, 5 };
 
         System.out.println (Arrays.toString (schoolmarks));
+
+        int [] num1 = { 1, 2, 3 };
+
+        int[] num2 = { 4, 5, 6 };
+
+        System.out.println (Arrays. Equals (num1, num2)); //false
+
+        Int [] num3 = {1, 2, 3};
+
+        System.out.println (Arrays.equals (num1, num3)); //true
     }
 }
