f34c576a95779449c4c6a3fced18ad385eea252b
Rose <rs8953363@gmail.com>
2022-06-13 23:50:10 -0500

    ...

---
+++ src/Arrays/CompareTwoArrays.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class CompareTwoArrays {
+    public static void main(String[] args) {
+        int [] num1 = { 1, 2, 3 };
+
+        int[] num2 = { 4, 5, 6 };
+
+        System.out.println (Arrays. Equals (num1, num2)); //false
+
+        Int [] num3 = {1, 2, 3};
+
+        System.out.println (Arrays.equals (num1, num3)); //true
+    }
+}
