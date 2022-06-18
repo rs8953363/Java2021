d4efb57d06a48e8c26091a1f154e556e0414dcc0
Rose <rs8953363@gmail.com>
2022-06-17 23:58:53 -0500

    ...

--- src/Arrays/CompareTwoArrays.java
+++ src/Arrays/CompareTwoArrays.java
@@ -1,19 +1,20 @@
 package Arrays;
 
+import java.util.Arrays;
 public class CompareTwoArrays {
     public static void main(String[] args) {
         int [] num1 = { 1, 2, 3 };
 
         int[] num2 = { 4, 5, 6 };
 
-        System.out.println (Arrays. Equals (num1, num2)); //false
+        System.out.println (Arrays.equals (num1, num2)); //false
 
-        Int [] num3 = {1, 2, 3};
+        int [] num3 = {1, 2, 3};
 
         System.out.println (Arrays.equals (num1, num3)); //true
 
         //Sort an Array
-        Int Marks [] = {12, 5, 7, 9};
+        int Marks [] = {12, 5, 7, 9};
 
         Arrays.sort(Marks);
 
