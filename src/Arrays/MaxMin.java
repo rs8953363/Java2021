fbf9a1073912929a05df9a62fa7ebf0a721884f3
Rose <rs8953363@gmail.com>
2022-06-26 03:25:04 -0500

    ...

---
+++ src/Arrays/MaxMin.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+
+public class MaxMin{
+
+    public static void largestAndSmallest(int[] numbers) {
+
+        int largest = Integer.MIN_VALUE;
+
+        int smallest = Integer.MAX_VALUE;
+
+        for (int number : numbers) {
+
+            if (number > largest) {
+
+                largest = number;
+
+            }
+
+            else if (number < smallest) {
+
+                smallest = number;
+
+            } }
+        System.out.println("Largest is : " + largest);
+
+        System.out.println("Smallest is : " + smallest); }
+}
+
