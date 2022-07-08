2a0d966da9b4f1334fac3d3e0941c88b24aaf9ec
Rose <rs8953363@gmail.com>
2022-07-07 23:21:14 -0500

    ...

---
+++ src/Arrays/MissingElement.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class MissingElement {
+    public static void main(String[] args) {
+
+        int ar [] = new int[]{1,2,3,5,6,7};
+        int n = ar.length+1;
+
+        int total = n*(n+1)/2;
+
+        for(int i =0;i<ar.length;i++){
+
+            total -=ar[i];}
+
+        System.out.println(total);
+    }
+}
