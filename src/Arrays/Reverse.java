12e7dd2cbb23daf6e5a0a7204b2c73641b75bb97
Rose <rs8953363@gmail.com>
2022-07-20 00:05:34 -0500

    ...\

---
+++ src/Arrays/Reverse.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+        String Array String[] s = new String[]{"My","Leg","is","cut"};
+
+        {String[] s = new String[]{"My","Leg","is","cut"};
+
+            for(int i=s.length-1; i>=0; i--){
+
+                System.out.println("reverse "+s[i]);}}
+    }
+}
